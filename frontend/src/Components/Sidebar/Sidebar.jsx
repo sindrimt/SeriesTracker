b901e026059298c9e3a5d5582cd4cc5788a77d40
Marcus Stephan Nordal <marcusn1301@gmail.com>
2022-05-25 22:13:30 +0200

    Added sidebar

---
+++ frontend/src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import './Sidebar.css';
+
+import { SidebarOuter, SearchOuter, CategoriesOuter, SidebarLine, PanelOuter, Icons, SidebarBottomLine, Trademark } from './SidebarStyles';
+
+import notifications from "../../Assets/Images/Sidebar/bell.svg";
+import dashboard from "../../Assets/Images/Sidebar/grid.svg";
+import messages from "../../Assets/Images/Sidebar/message-square.svg";
+import settings from "../../Assets/Images/Sidebar/settings.svg";
+import friends from "../../Assets/Images/Sidebar/users.svg";
+
+const Sidebar = () => {
+    return (
+        <>
+            <SidebarOuter>
+                <SearchOuter>
+                    Search series
+                    <form className="searchForm" onSubmit={(e) => e.preventDefault()}>
+                        <label htmlFor="searchSeries">Search series</label>
+                        <input
+                            id="searchSeries"
+                            type="text"
+                            role="searchbox"
+                            placeholder="One Piece"
+                            value=""
+                        />
+
+                    </form>
+                </SearchOuter>
+                <CategoriesOuter>
+                    <ul className='categoryParent'>
+                        <li>
+                            Categories
+                        </li>
+                        <ul className='category'>
+                            <li>
+                                Action
+                            </li>
+                            <li>
+                                Anime
+                            </li>
+                            <li>
+                                Fiction
+                            </li>
+                            <li>
+                                View more ...
+                            </li>
+                        </ul>
+                    </ul>
+                </CategoriesOuter>
+                <SidebarLine></SidebarLine>
+                <PanelOuter>
+                    <ul>
+                        <li>
+                            <Icons src={dashboard} alt="Dashboard icon" />
+                            Dashboard
+                        </li>
+                        <li>
+                            <Icons src={friends} alt="Friends icon" />
+                            Friends
+                        </li>
+                        <li>
+                            <Icons src={messages} alt="Messages icon" />
+                            Direct Messages
+                        </li>
+                        <li>
+                            <Icons src={notifications} alt="Notifications icon" />
+                            Notifications
+                        </li>
+                        <li>
+                            <Icons src={settings} alt="Settings icon" />
+                            Settings
+                        </li>
+                    </ul>
+                </PanelOuter>
+                <SidebarBottomLine/>
+                <Trademark>
+                    SeriesTracker 2022&reg; corporation
+                </Trademark>
+            </SidebarOuter>
+
+        </>
+    );
+};
+
+export default Sidebar;
