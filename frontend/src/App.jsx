74b7310d534a814ec4e12e22bd57884a7282083b
sindrimt <sindrimt@gmail.com>
2022-05-28 19:10:55 +0200

    e

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Content from "./Components/Content/Content.jsx";
 import Login from "./Pages/LoginPage/Login.jsx";
 
 import "./Styles/app.css";
+import ContentRight from "./Components/ContentRight/ContentRight.jsx";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <Navbar />
       <Sidebar />
       <Content />
+      <ContentRight />
     </>
   );
 };
