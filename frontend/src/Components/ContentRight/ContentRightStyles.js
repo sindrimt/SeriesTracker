fe5c9d112662deaa59d7f09d2f0ebe6034e77e78
sindrimt <sindrimt@gmail.com>
2022-05-28 19:11:13 +0200

    Added Content right and styling
    Co-authored-by: marcusn1301 <marcusn1301@users.noreply.github.com>

---
+++ frontend/src/Components/ContentRight/ContentRightStyles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const ContentRightOuter = styled.div`
+  position: fixed;
+  margin: 110px 0px 0px 0px;
+  width: 20%;
+  height: 100%;
+  right: 0;
+  background-color: lightgrey;
+`;
