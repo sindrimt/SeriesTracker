b75607706fc3d7cc732556132b6e3c9cd760fdf4
Marcus Stephan Nordal <marcusn1301@gmail.com>
2022-05-29 18:57:56 +0200

    Created profile page.
    Closes #8

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./Components/Navbar/Navbar.jsx";
 import Sidebar from "./Components/Sidebar/Sidebar.jsx";
 import Content from "./Components/Content/Content.jsx";
 import Login from "./Pages/LoginPage/Login.jsx";
-
+import Profile from "./Pages/ProfilePage/Profile.jsx";
 import "./Styles/app.css";
 import ContentRight from "./Components/ContentRight/ContentRight.jsx";
 
@@ -16,6 +16,7 @@ const App = () => {
       <Sidebar />
       <Content />
       <ContentRight />
+      {/* <Profile /> */}
     </>
   );
 };
