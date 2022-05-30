db2d83ecfa973b6a258552212e9340c1e6b50780
Marcus Stephan Nordal <marcusn1301@gmail.com>
2022-05-30 16:45:33 +0200

    Added grid and button to login page
    Related to #13

---
+++ frontend/src/Components/Buttons/FormButton/FormButton.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { 
+    FButton, 
+
+} from "./FormButtonStyles";
+
+const FormButton = ({buttonText}) => {
+  return (
+
+    <FButton>{buttonText}</FButton>
+  );
+};
+
+export default FormButton;
--- frontend/src/Pages/LoginPage/LoginStyles.js
+++ frontend/src/Pages/LoginPage/LoginStyles.js
@@ -1,27 +1,40 @@
 import styled from "styled-components";
 
-export const LoginOuterContainer = styled.div``;
+export const LoginOuterContainer = styled.div`
+  position: absolute;
+  width: 100%;
+  padding-right: 2vw;
+  padding-left: 2vw;
+  height: 100vh;
+  
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  grid-column-gap: 20px;
+  grid-row-gap: 20px;
+  padding-bottom: 100px;
+
+  @media (max-width: 1250px) {
+    grid-template-columns: 1fr;
+    grid-row-gap: 0px;
+  }
+`;
 
 export const LoginPreviewOuter = styled.div`
-  position: absolute;
-  left: 6%;
-  width: 40%;
-  height: 500px;
-  @media (min-width: 850px) {
-    &:after {
+  justify-self: center;
+  align-self: center;
+  
+  @media (min-width: 1250px) {
+    
+    &:after{
       content: "";
       position: absolute;
-      height: calc(30vw + 15%);
-      border: 1px solid rgba(0, 0, 0, 0.2);
-      margin-left: 100px;
-      top: 80px;
+      height: 60%;
+      border-right: 1px solid rgba(0, 0, 0, 0.2);
+      margin-left: 5%;
+      top: 130px;
       border-radius: 5px;
     }
-  }
 
-  @media (max-width: 850px) {
-    width: calc(70%);
-    left: 14%;
   }
 `;
 export const TxtOuter = styled.div`
@@ -34,46 +47,62 @@ export const TxtOuter = styled.div`
 `;
 
 export const LoginPreviewHeader = styled.div`
-  font-size: calc(1.2vw + 5px);
+  font-size: 24px;
   color: #a63fcb;
   margin-bottom: 3px;
-
-  @media (max-width: 850px) {
-    font-size: calc(1.8vw + 5px);
-  }
 `;
 
 export const LoginPreviewTxt = styled.div`
-  font-size: calc(0.8vw + 5px);
+  font-size: 18px;
   width: 70%;
   text-align: center;
   margin-bottom: 7px;
   font-weight: 200;
   color: #393939;
-
-  @media (max-width: 850px) {
-    font-size: calc(1.2vw + 5px);
-  }
 `;
 
 export const LoginPreviewImg = styled.img`
-  width: 100%;
+  width: 700px;
   object-fit: fill;
   border-radius: 3px;
   filter: drop-shadow(0px 6px 4px rgba(0, 0, 0, 0.25));
+  
+  @media (max-width: 1250px) {
+    display: none;
+  }
 `;
 
+export const LoginBoxOuter = styled.div`
+  margin-top: 150px;
+  display: grid;
+  height: 500px;
+  grid-template-rows: 8fr 2fr;
+  grid-row-gap: 20px;
+  justify-content: center;
+  
+  @media (max-width: 1250px) {
+    margin-top: -30px;
+  }
+`
 export const LoginBoxMain = styled.div`
   display: flex;
   flex-direction: column;
-  position: absolute;
-  background-color: white;
-  align-items: center;
-  top: 150px;
-  right: 23%;
-  border: 1px solid gray;
+  align-items: center; 
+  justify-self: center;
   width: 380px;
-  height: 540px;
+  box-shadow: rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;
+  border-radius: 5px;
+  padding-bottom: 20px;
+`;
+
+export const LoginBoxSecondary = styled.div`
+  display: flex;
+  width: 380px;
+  height: 100%;
+  box-shadow: rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;
+  border-radius: 5px;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const LoginBoxLogo = styled.img`
@@ -86,25 +115,24 @@ export const LoginBoxLogoOuter = styled.div`
   align-items: center;
   width: 100%;
   height: 100px;
-  background-color: blue;
 `;
 
 export const LoginFields = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr;
+  width: 60%;
+  grid-row-gap: 10px;
   justify-content: center;
   align-items: center;
-  background-color: green;
   height: 200px;
+  margin-bottom: 20px;
 `;
 
-export const LoginButton = styled.div``;
+export const LoginButton = styled.div`
+`;
 
 export const LoginForgot = styled.div``;
 
 export const LoginBoxLine = styled.div``;
 
 export const LoginBoxGoogle = styled.div``;
-
-export const LoginBoxSecondary = styled.div``;
