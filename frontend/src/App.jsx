a8fe35f36f86c3f86b79fc2ac301cafb451b6406
sindrimt <sindrimt@gmail.com>
2022-05-31 13:09:05 +0200

    goof

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -34,15 +34,3 @@ const App = () => {
 };
 
 export default App;
-{
-  /*  {currentUser ? (
-        <>
-          <Navbar />
-          <Sidebar />
-          <Content />
-          <ContentRight />
-        </>
-      ) : (
-        <Login />
-      )} */
-}
