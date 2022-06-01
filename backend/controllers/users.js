c795ff1d1291d7434acae87615b702321889608a
sindrimt <sindrimt@gmail.com>
2022-06-01 23:28:35 +0200

    getUser and postUser

---
+++ backend/controllers/users.js
@@ -0,0 +1,23 @@
+import UserData from "../models/UserData.js";
+
+export const getUser = async (req, res) => {
+  try {
+    const user = await UserData.find();
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createUser = async (req, res) => {
+  const user = req.body;
+  const newUser = new UserData(user);
+  try {
+    await newUser.save();
+
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
