e4c1cf5422279a46e4fc1474a08d2a978fbdf5af
sindrimt <sindrimt@gmail.com>
2022-06-03 20:10:27 +0200

    axios put method
    Co-authored-by: marcusn1301 <marcusn1301@users.noreply.github.com>

--- frontend/src/axios/axios.js
+++ frontend/src/axios/axios.js
@@ -1,18 +1,25 @@
 import axios from "axios";
 
 const apiUrlUsers = "/api/users";
+const apiUrlUsersId = "/api/users/2342sfs1312321"; //todo ID ONLY FOR TEST
 
 const apiUrlSeries = "/api/series";
 
 export const getUser = () =>
   axios
-    .get(apiUrlUsers)
+    .get("/api/users/2342sfs1312321")
     .then((response) => console.log(response))
     .catch((error) => console.log(error));
 
 export const postUser = (postUser) =>
   axios
     .post(apiUrlUsers, postUser)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+
+export const putUser = (data) =>
+  axios
+    .put(apiUrlUsersId, data)
     .then((res) => console.log(res))
     .catch((err) => console.log(err));
 
