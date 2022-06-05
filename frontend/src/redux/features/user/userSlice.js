8373fcb1c311457b192fc398e127496b4186818f
sindrimt <sindrimt@gmail.com>
2022-06-05 23:37:01 +0200

    Added redux actions for logging in and logging out
    Closes #20
    
    Co-authored-by: marcusn1301 <marcusn1301@users.noreply.github.com>

---
+++ frontend/src/redux/features/user/userSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: {},
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    loginUser: (state, action) => {
+      state.user = action.payload;
+    },
+    logoutUser: (state) => {
+      state.user = {};
+    },
+  },
+});
+
+export const { loginUser, logoutUser } = userSlice.actions;
+
+export default userSlice.reducer;
