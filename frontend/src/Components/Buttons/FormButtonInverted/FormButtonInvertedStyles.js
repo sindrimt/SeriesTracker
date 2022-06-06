fad79c8be9ad3b4ad44b1e2d18fd4f7925fa82ee
Marcus Stephan Nordal <marcusn1301@gmail.com>
2022-06-06 21:37:47 +0200

    Changes to form button styling

--- frontend/src/Components/Buttons/FormButtonInverted/FormButtonInvertedStyles.js
+++ frontend/src/Components/Buttons/FormButtonInverted/FormButtonInvertedStyles.js
@@ -7,9 +7,10 @@ export const FButtonInverted = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    background: linear-gradient(178.54deg, #ffffff 1.24%, rgba(166, 63, 203, 0) 342.56%);
+    background: linear-gradient(178.54deg, rgba(166, 63, 203, 0.05) 1.24%, rgba(166, 63, 203, 0) 342.56%);
     filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.04)) drop-shadow(0px 2px 4px rgba(0, 0, 0, 0.16));
-border-radius: 10px;
+    border-radius: 10px;
+    border: 1px lightgray solid;
 
     &:hover {
         cursor: pointer;
