a64a57ce334adc4ae83f8317a02dd49f701e4c49
sindrimt <sindrimt@gmail.com>
2022-06-06 21:16:37 +0200

    multer integration
    Related to #21

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,25 @@
 import express from "express";
 
-import { getUser, createUser, getUserById } from "../controllers/users.js";
+import { getUser, createUser, getUserById, patchUserById } from "../controllers/users.js";
+
+import multer from "multer";
+
+const fileStorageEngine = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, "./uploads/");
+  },
+
+  filename: function (req, file, callback) {
+    callback(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({
+  storage: fileStorageEngine,
+  limits: {
+    fileSize: 1024 * 1024 * 5,
+  },
+});
 
 // Denne filen inneholder alle metodene / verbene til users
 const router = express.Router();
@@ -11,8 +30,10 @@ router.get("/users", getUser);
 
 router.get("/users/:id", getUserById);
 
-router.put("/users/:id", getUserById);
+router.put("/users/:id", patchUserById);
 
-router.post("/users", createUser);
+router.patch("/users/:id", patchUserById);
+
+router.post("/users", upload.single("image"), createUser);
 
 export default router;
