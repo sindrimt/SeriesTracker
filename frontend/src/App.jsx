0f92ae83ad4ad938b946a76b330119ab216b9279
sindrimt <sindrimt@gmail.com>
2022-06-06 10:56:32 +0200

    Added aving to currentuser to app insted of homepage
    This results in saving the globalUser only on refresh, not on every render of homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -14,8 +14,43 @@ import CreatePost from "./Components/CreatePost/CreatePost.jsx";
 
 import { useAuth } from "./firebase.js";
 
+import { useSelector, useDispatch } from "react-redux";
+import { loginUser, logoutUser } from "./redux/features/user/userSlice";
+
+import axios from "axios";
+
 const App = () => {
   const currentUser = useAuth();
+  const [done, setDone] = useState(false);
+
+  const globalUser = useSelector((state) => state.user.user);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (currentUser) {
+      console.log(currentUser);
+      fetchUser();
+    } else {
+      setTimeout(200);
+      setDone(!done);
+    }
+  }, [done]);
+
+  const fetchUser = () => {
+    console.log("=================================================");
+    console.log("CURRENTUSER IS SET FROM APP HERE");
+    console.log("=================================================");
+
+    axios
+      .get(`api/users/${currentUser?.uid}`)
+      .then(({ data }) => {
+        console.log(data);
+        dispatch(loginUser(data));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <BrowserRouter>
