e9a9159eb678aba2166a84105b46f34cfea50db3
Marcus Stephan Nordal <marcusn1301@gmail.com>
2022-06-07 21:21:54 +0200

    Created grid container and styled anime cards
    Co-authored-by: Sindrimt <sindrimt@users.noreply.github.com>

--- frontend/src/Components/CreatePost/CreatePostStyles.js
+++ frontend/src/Components/CreatePost/CreatePostStyles.js
@@ -2,7 +2,21 @@ import styled from "styled-components";
 
 export const CreatePostOuter = styled.div`
   position: absolute;
-  width: 100%;
+  justify-content: center;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
   height: 80%;
-  margin-top: 10%;
+  margin-top: 180px;
+
+`;
+
+export const Gridcontainer = styled.div`
+  padding: 10px;
+  width: 70%;
+  height: 120px;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+  row-gap: 1rem;
+  column-gap: 2rem;
 `;
