ea6295fac33b1383ff2f2397cc8c9d3211aacfdd
Marcus Stephan Nordal <marcusn1301@gmail.com>
2022-06-07 21:25:17 +0200

    Added overlay pop up Related to #30
    Co-authored-by: Sindrimt <sindrimt@users.noreply.github.com>

---
+++ frontend/src/Components/Overlay/Overlay.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { OverlayOuter } from "./OverlayStyles";
+
+const Overlay = () => {
+  return <OverlayOuter>asdsa</OverlayOuter>;
+};
+
+export default Overlay;
---
+++ frontend/src/Components/Overlay/OverlayStyles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const OverlayOuter = styled.div`
+    position: absolute;
+    width: 40vw;
+    height: 40vh;
+    top: 30%;
+    left: 30%;
+    background-color: lightskyblue;
+    z-index: 10000000;
+`;
