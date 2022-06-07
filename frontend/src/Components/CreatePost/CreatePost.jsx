c470a4be73d3cfaafd4f358f2308609babda9a70
Marcus Stephan Nordal <marcusn1301@gmail.com>
2022-06-07 21:19:13 +0200

    Created anime card container & anime card. Related to #29
    Co-authored-by: Sindrimt <sindrimt@users.noreply.github.com>

--- frontend/src/Components/CreatePost/CreatePost.jsx
+++ frontend/src/Components/CreatePost/CreatePost.jsx
@@ -1,35 +1,89 @@
+import axios from "axios";
 import React, { useRef, useState, useEffect } from "react";
-import { CreatePostOuter } from "./CreatePostStyles";
+import { CreatePostOuter, Gridcontainer } from "./CreatePostStyles";
+
+import Loading from "../../Pages/LoadingPage/Loading";
+
+import AnimeCard from "../Cards/AnimeCard/AnimeCard";
+
+import Overlay from "../Overlay/Overlay";
 
 const CreatePost = () => {
-  const [image, setImage] = useState({});
+  const [topAnime, setTopAnime] = useState([]);
 
-  const fileOnChange = (e) => {
-    setImage(e.target.files[0]);
-  };
+  const [loading, setLoading] = useState(false);
 
-  const sendImage = (e) => {
-    let formdata = new FormData();
+  const [form, setForm] = useState(false);
 
-    formdata.append("image", image);
-    formdata.append("_id", "asdaas76345345");
+  const [show, setShow] = useState(false);
 
-    fetch("api/users", {
-      method: "POST",
-      body: formdata,
-    })
-      .then((res) => res.text())
-      .then((resBody) => {
-        console.log(resBody);
+  const GetTopAnime = () => {
+    setLoading(true);
+    axios
+      .get(`https://api.jikan.moe/v3/top/anime/1/bypopularity`)
+      //https://api.jikan.moe/v3/top/type/page/subtype
+      .then(({ data }) => {
+        setTopAnime(data.top.slice(0, 50));
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
       });
   };
 
-  return (
-    <CreatePostOuter>
-      <input type="file" onChange={fileOnChange} />
+  useEffect(() => {
+    GetTopAnime();
+  }, []);
 
-      <button onClick={sendImage}>Upload</button>
-    </CreatePostOuter>
+  console.log(topAnime);
+
+  // const fileOnChange = (e) => {
+  //   setImage(e.target.files[0]);
+  // };
+
+  // const sendImage = (e) => {
+  //   let formdata = new FormData();
+
+  //   formdata.append("image", image);
+  //   formdata.append("_id", "asdaas76345345");
+
+  //   fetch("api/users", {
+  //     method: "POST",
+  //     body: formdata,
+  //   })
+  //     .then((res) => res.text())
+  //     .then((resBody) => {
+  //       console.log(resBody);
+  //     });
+  // };
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  console.log(show);
+  return (
+    <>
+      {show ? <Overlay /> : <></>}
+
+      <CreatePostOuter>
+        {/* <input type="file" onChange={fileOnChange} />
+
+      <button onClick={sendImage}>Upload</button> */}
+
+        <Gridcontainer>
+          {topAnime.map((anime, index) => {
+            return (
+              <>
+                <div>
+                  <AnimeCard title={anime.title} episodes={anime.episodes} image={anime.image_url} />
+                </div>
+              </>
+            );
+          })}
+        </Gridcontainer>
+      </CreatePostOuter>
+    </>
   );
 };
 
