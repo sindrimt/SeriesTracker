57a4ec04de722a8e9868733ffab5896cd3257027
sindrimt <sindrimt@gmail.com>
2022-06-13 22:58:43 +0200

    Breaking changes
    Closes #52
    
    Co-authored-by: marcusn1301 <marcusn1301@users.noreply.github.com>

--- frontend/src/Components/Navbar/NavbarStyles.js
+++ frontend/src/Components/Navbar/NavbarStyles.js
@@ -22,8 +22,8 @@ export const NavbarLine = styled.div`
   transition: 0.3s ease;
   top: 110px;
   border-radius: 5px;
-  //left: 5%;
-  width: 100vw;
+  //left: 3%;
+  width: 100%;
   border-top: ${(props) =>
     props.showLine ? "1.5px solid rgba(137, 137, 137, 0)" : `1.5px solid ${props.theme.navbar.line}`};
 `;
