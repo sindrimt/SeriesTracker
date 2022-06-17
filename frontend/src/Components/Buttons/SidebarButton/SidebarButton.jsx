d00035d009f3694d0bfed0ad1e1d185a4517ff4b
sindrimt <sindrimt@gmail.com>
2022-06-17 18:42:04 +0200

    Sidebat button component
    Co-authored-by: marcusn1301 <marcusn1301@users.noreply.github.com>

---
+++ frontend/src/Components/Buttons/SidebarButton/SidebarButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { PanelItem, Text, Icon } from "./SidebarButtonStyles";
+
+const SidebarButton = ({ text, icon }) => {
+  return (
+    <PanelItem>
+      <Icon src={icon} />
+      <Text>{text}</Text>
+    </PanelItem>
+  );
+};
+
+export default SidebarButton;
