8f2cb906bde94082871780d93ac1b10dd99913ab
sindrimt <sindrimt@gmail.com>
2022-06-19 22:39:57 +0200

    Sidebar item styling
    Co-authored-by: marcusn1301 <marcusn1301@users.noreply.github.com>

--- frontend/src/Components/Buttons/SidebarButton/SidebarButtonStyles.js
+++ frontend/src/Components/Buttons/SidebarButton/SidebarButtonStyles.js
@@ -2,14 +2,23 @@ import styled from "styled-components";
 
 export const PanelItem = styled.div`
   width: 100%;
+  height: 50px;
   display: flex;
   padding: 0 0 0 20px;
+  border-radius: 0 10px 10px 0;
   align-items: center;
   transition: 0.15s;
+  border-left: 5px solid transparent;
+
+  //background-color: rgb(45, 42, 45);
+
+  &:nth-child(1) {
+    border-left: 5px solid ${(props) => props.theme.sidebar.buttonBorder};
+    background-color: rgb(45, 42, 45);
+  }
 
   &:hover {
-    background-color: lightgoldenrodyellow;
-    color: black;
+    background-color: rgb(75, 72, 75);
     cursor: pointer;
   }
 
@@ -28,5 +37,5 @@ export const Icon = styled.img`
   column-gap: 20px;
   padding: 0 15px 0 0;
   top: 6px;
-  width: 37px;
+  width: 35px;
 `;
