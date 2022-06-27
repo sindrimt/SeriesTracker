f3fca61ea51859ab4b0eed1f5bf0d0145c525b92
sindrimt <sindrimt@gmail.com>
2022-06-27 20:48:55 +0200

    Some styling

--- frontend/src/Components/Content/ContentStyles.js
+++ frontend/src/Components/Content/ContentStyles.js
@@ -48,11 +48,18 @@ export const PickUp = styled.div`
   font-weight: 300;
   white-space: nowrap;
   align-items: center;
+  justify-content: space-between;
   color: ${(props) => props.theme.navbar.fontColor2};
+
+  .pickupSpan {
+    @media (max-width: 1060px) {
+      display: none;
+    }
+  }
 `;
 
 export const Filter = styled.img`
-  margin-left: 20px;
+  margin-left: 10px;
   width: 20px;
   color: ${(props) => props.theme.content.filter};
 `;
@@ -66,11 +73,38 @@ export const GridTypeIcons = styled.div`
   position: relative;
   display: flex;
   justify-content: flex-end;
-  width: 100%;
+
   height: 30px;
-  margin-right: 40px;
+  margin-right: 25px;
 
   &:hover {
     cursor: pointer;
   }
+`;
+
+export const SearchOuter = styled.input`
+  position: relative;
+  display: flex;
+  background-color: ${(props) => props.theme.contentRight.accentColor};
+  height: 35px;
+  width: 100%;
+  font-size: 16px;
+  padding: 10px;
+  border-bottom: 2px solid black;
+  border-left: 1px solid black;
+  border-right: 1px solid black;
+  border-top: 1px solid black;
+  box-shadow: 0px 4px 4px rgba(100, 100, 100, 0.1);
+  text-overflow: ellipsis;
+  margin-left: 5px;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+export const SearchOuterContainer = styled.div`
+  height: 35px;
+  display: flex;
+  width: 37%;
 `;
