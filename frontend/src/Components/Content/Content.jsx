8409867f2185351aa911e06cd42546d0cb5a63d1
sindrimt <sindrimt@gmail.com>
2022-08-21 14:36:44 +0200

    epic

--- frontend/src/Components/Content/Content.jsx
+++ frontend/src/Components/Content/Content.jsx
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from "react";
 
 import {
-  ContentContainerOuter,
-  PickUp,
-  Filter,
-  GridTypeIcons,
-  Icon,
-  ContentContainerGrid,
-  ContentContainerGridIcons,
-  SearchOuter,
-  SearchOuterContainer,
+    ContentContainerOuter,
+    PickUp,
+    Filter,
+    GridTypeIcons,
+    Icon,
+    ContentContainerGrid,
+    ContentContainerGridIcons,
+    SearchOuter,
+    SearchOuterContainer,
 } from "./ContentStyles";
 
 import Loading from "../../Pages/LoadingPage/Loading";
@@ -40,98 +40,91 @@ import { useSelector } from "react-redux";
 import { useAuth } from "../../firebase";
 
 const Content = ({ series }) => {
-  const [iconView, setIconView] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [seriesArray, setSeriesArray] = useState(series);
-  const [filtered, setFiltered] = useState(series);
-  const [searchTerm, setSearchTerm] = useState("");
+    const [iconView, setIconView] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [seriesArray, setSeriesArray] = useState(series);
+    const [filtered, setFiltered] = useState(series);
+    const [searchTerm, setSearchTerm] = useState("");
 
-  const colorTheme = useSelector((state) => state.theme.theme);
-  const currentUser = useAuth();
+    const colorTheme = useSelector((state) => state.theme.theme);
+    const currentUser = useAuth();
 
-  useEffect(() => {
-    let filter = seriesArray.filter((serie) => {
-      return serie?.title?.toLowerCase().includes(searchTerm.toLowerCase());
-    });
-    setFiltered(filter);
-  }, [searchTerm]);
+    useEffect(() => {
+        console.log("e");
+        let filter = seriesArray.filter((serie) => {
+            return serie?.title?.toLowerCase().includes(searchTerm.toLowerCase());
+        });
+        setFiltered(filter);
+    }, [searchTerm]);
 
-  return (
-    <>
-      <ContentContainerOuter>
-        <PickUp>
-          <span className="pickupSpan">Pick up where you left</span>
-          <SearchOuterContainer>
-            <form style={{ width: "100%" }}>
-              <SearchOuter type="search" placeholder="search" onChange={(e) => setSearchTerm(e.target.value)} />
-            </form>
-            <Filter src={colorTheme === "light" ? filter : colorTheme === "dark" ? filter_darkmode : filter_hotdog} />
-          </SearchOuterContainer>
-          <GridTypeIcons>
-            <Icon src={colorTheme === "light" ? rowView : colorTheme === "dark" ? rowView_darkmode : rowView_hotdog} />
-            <Icon
-              src={
-                colorTheme === "light"
-                  ? iconViewIcon
-                  : colorTheme === "dark"
-                  ? iconViewIcon_darkmode
-                  : iconViewIcon_hotdog
-              }
-              onClick={() => setIconView(!iconView)}
-            />
-          </GridTypeIcons>
-        </PickUp>
-        {iconView ? (
-          <ContentContainerGridIcons>
-            {filtered?.length === 0 ? (
-              <span style={{ fontSize: "18px", paddingTop: "20px" }}>No results!</span>
-            ) : (
-              filtered?.map((serie) => (
-                <>
-                  <GridCard
-                    title={serie?.title}
-                    episodesWatched={serie?.episodesWatched}
-                    episodeCount={serie?.episodeCount}
-                    rating={serie?.rating}
-                    image={serie?.photoUrl}
-                    watchTime={serie?.watchTime}
-                  />
-                </>
-              ))
-            )}
-          </ContentContainerGridIcons>
-        ) : (
-          <ContentContainerGrid>
-            {filtered?.length === 0 ? (
-              <span style={{ fontSize: "18px", paddingTop: "20px" }}>No results!</span>
-            ) : (
-              filtered?.map((serie) => (
-                <>
-                  <HomepageCard
-                    title={serie?.title}
-                    episodesWatched={serie?.episodesWatched}
-                    episodeCount={serie?.episodeCount}
-                    rating={serie?.rating}
-                    image={serie?.photoUrl}
-                    watchTime={serie?.watchTime}
-                    description={
-                      serie?.description
-                        ? serie?.description
-                        : "Gol D. Roger obtained everything the world had to offer ..."
-                    }
-                  />
-                </>
-              ))
-            )}
-          </ContentContainerGrid>
-        )}
-      </ContentContainerOuter>
-      <Link to="/createPost">
-        {/*  //TODO Add series button was here */}
-        <AddSerieButton />
-      </Link>
-    </>
-  );
+    return (
+        <>
+            <ContentContainerOuter>
+                <PickUp>
+                    <span className="pickupSpan">Pick up where you left</span>
+                    <SearchOuterContainer>
+                        <form style={{ width: "100%" }}>
+                            <SearchOuter type="search" placeholder="search" onChange={(e) => setSearchTerm(e.target.value)} />
+                        </form>
+                        <Filter src={colorTheme === "light" ? filter : colorTheme === "dark" ? filter_darkmode : filter_hotdog} />
+                    </SearchOuterContainer>
+                    <GridTypeIcons>
+                        <Icon src={colorTheme === "light" ? rowView : colorTheme === "dark" ? rowView_darkmode : rowView_hotdog} />
+                        <Icon
+                            src={colorTheme === "light" ? iconViewIcon : colorTheme === "dark" ? iconViewIcon_darkmode : iconViewIcon_hotdog}
+                            onClick={() => setIconView(!iconView)}
+                        />
+                    </GridTypeIcons>
+                </PickUp>
+                {iconView ? (
+                    <ContentContainerGridIcons>
+                        {filtered?.length === 0 ? (
+                            <span style={{ fontSize: "18px", paddingTop: "20px" }}>No results!</span>
+                        ) : (
+                            filtered?.map((serie) => (
+                                <>
+                                    <GridCard
+                                        title={serie?.title}
+                                        episodesWatched={serie?.episodesWatched}
+                                        episodeCount={serie?.episodeCount}
+                                        rating={serie?.rating}
+                                        image={serie?.photoUrl}
+                                        watchTime={serie?.watchTime}
+                                    />
+                                </>
+                            ))
+                        )}
+                    </ContentContainerGridIcons>
+                ) : (
+                    <ContentContainerGrid>
+                        {filtered?.length === 0 ? (
+                            <span style={{ fontSize: "18px", paddingTop: "20px" }}>No results!</span>
+                        ) : (
+                            filtered?.map((serie) => (
+                                <>
+                                    <HomepageCard
+                                        title={serie?.title}
+                                        episodesWatched={serie?.episodesWatched}
+                                        episodeCount={serie?.episodeCount}
+                                        rating={serie?.rating}
+                                        image={serie?.photoUrl}
+                                        watchTime={serie?.watchTime}
+                                        description={
+                                            serie?.description ? serie?.description : "Gol D. Roger obtained everything the world had to offer ..."
+                                        }
+                                    />
+                                </>
+                            ))
+                        )}
+                    </ContentContainerGrid>
+                )}
+            </ContentContainerOuter>
+            <Link to="/createPost">
+                {/*  //TODO Add series button was here */}
+                <AddSerieButton />
+            </Link>
+        </>
+    );
 };
 
 export default Content;
