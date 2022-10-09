ecedb2935c9a114a4dcfb3facdae5c3ccd96d9be
Marcus Stephan Nordal <marcusn1301@gmail.com>
2022-10-09 16:05:46 +0200

    #76 Scaling adjustment: sidebar

--- frontend/src/Components/Sidebar/Sidebar.jsx
+++ frontend/src/Components/Sidebar/Sidebar.jsx
@@ -2,17 +2,17 @@ import React, { useState, useEffect } from "react";
 import "./Sidebar.css";
 
 import {
-    SidebarOuter,
-    SearchOuter,
-    SidebarLine,
-    PanelOuter,
-    Icons,
-    SidebarBottomLine,
-    Trademark,
-    AccountHeader,
-    PanelOuterAccount,
-    Github,
-    GithubBack,
+  SidebarOuter,
+  SearchOuter,
+  SidebarLine,
+  PanelOuter,
+  Icons,
+  SidebarBottomLine,
+  Trademark,
+  AccountHeader,
+  PanelOuterAccount,
+  Github,
+  GithubBack,
 } from "./SidebarStyles";
 
 import { AiOutlineSearch } from "react-icons/ai";
@@ -34,64 +34,64 @@ import { useNavigate } from "react-router-dom";
 import useWindowDimensions from "../../Hooks/useWindowDimensions";
 
 const Sidebar = () => {
-    const [showSidebar, setShowSidebar] = useState(false);
-    let navigate = useNavigate();
-    const scrollPosition = useScroll();
+  const [showSidebar, setShowSidebar] = useState(false);
+  let navigate = useNavigate();
+  const scrollPosition = useScroll();
 
-    const { width } = useWindowDimensions();
+  const { width } = useWindowDimensions();
 
-    let fixed = false;
+  let fixed = false;
 
-    if (scrollPosition > 38) {
-        fixed = true;
+  if (scrollPosition > 38) {
+    fixed = true;
+  }
+
+  useEffect(() => {
+    if (width >= 720) {
+      setShowSidebar(true);
+    } else {
+      setShowSidebar(false);
     }
+  }, [width]);
 
-    useEffect(() => {
-        if (width >= 640) {
-            setShowSidebar(true);
-        } else {
-            setShowSidebar(false);
-        }
-    }, [width]);
-
-    return (
-        <>
-            <VscMenu className="menuButton" onClick={() => setShowSidebar(!showSidebar)} size={25} />
-            {showSidebar && (
-                <SidebarOuter isFixed={fixed}>
-                    {/*  <SearchOuter>
+  return (
+    <>
+      <VscMenu className="menuButton" onClick={() => setShowSidebar(!showSidebar)} size={25} />
+      {showSidebar && (
+        <SidebarOuter isFixed={fixed}>
+          {/*  <SearchOuter>
           Search series
           <form className="searchForm" onSubmit={/*) => e.preventDefault()}>
             <label htmlFor="searchSeries">Search series</label>
             <input id="searchSeries" type="text" role="searchbox" placeholder="Search" />
           </form>
         </SearchOuter> */}
-                    <PanelOuter>
-                        <SidebarButton icon={dashboard} text="Dashboard" />
-                        {/* <SidebarButton icon={notifications} text="Notifications" /> */}
-                        <SidebarButton icon={notifications} text="Find Series" clickAction={() => navigate("/create-post")} />
-                        <SidebarButton icon={messages} text="Messages" />
-                        <SidebarButton icon={friends} text="Friends" />
-                        <SidebarButton icon={settings} text="Settings" />
-                        <SidebarLine />
-                    </PanelOuter>
-                    <PanelOuterAccount>
-                        <AccountHeader>Account</AccountHeader>
-                        <SidebarButton icon={analytics} text="Stats" />
-                        <SidebarButton icon={settings} text="Updates" />
-                        <SidebarButton icon={analytics} text="Trending" />
-                        <SidebarButton icon={settings} text="Settings" />
-                    </PanelOuterAccount>
-                    <a href="https://github.com/sindrimt/SeriesTracker">
-                        <GithubBack>
-                            <Github src={github} />
-                        </GithubBack>
-                    </a>
-                    <Trademark>SeriesTracker 2022&reg;</Trademark>
-                </SidebarOuter>
-            )}
-        </>
-    );
+          <PanelOuter>
+            <SidebarButton icon={dashboard} text="Dashboard" />
+            {/* <SidebarButton icon={notifications} text="Notifications" /> */}
+            <SidebarButton icon={notifications} text="Find Series" clickAction={() => navigate("/create-post")} />
+            <SidebarButton icon={messages} text="Messages" />
+            <SidebarButton icon={friends} text="Friends" />
+            <SidebarButton icon={settings} text="Settings" />
+            <SidebarLine />
+          </PanelOuter>
+          <PanelOuterAccount>
+            <AccountHeader>Account</AccountHeader>
+            <SidebarButton icon={analytics} text="Stats" />
+            <SidebarButton icon={settings} text="Updates" />
+            <SidebarButton icon={analytics} text="Trending" />
+            <SidebarButton icon={settings} text="Settings" />
+          </PanelOuterAccount>
+          <a href="https://github.com/sindrimt/SeriesTracker">
+            <GithubBack>
+              <Github src={github} />
+            </GithubBack>
+          </a>
+          <Trademark>SeriesTracker 2022&reg;</Trademark>
+        </SidebarOuter>
+      )}
+    </>
+  );
 };
 
 export default Sidebar;
