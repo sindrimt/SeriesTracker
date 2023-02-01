97225a8bf0959954f7bb15eb4b8ac1a960f9b16b
sindrimt <sindrimt@gmail.com>
2023-02-01 14:18:43 +0100

    new add and delete buttons

---
+++ frontend/src/Components/Buttons/EpisodeButtons/AddButton/AddButton.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./addbutton.css";
+
+const AddButton = () => {
+    return (
+        <div className="">
+            <button className="icon-btn add-btn">
+                <div className="add-icon"></div>
+                <div className="btn-txt">Add</div>
+            </button>
+        </div>
+    );
+};
+
+export default AddButton;
---
+++ frontend/src/Components/Buttons/EpisodeButtons/DeleteButton/DeleteButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./deletebutton.css";
+
+const DeleteButton = () => {
+    return (
+        <div className="">
+            <button className="icon-btn add-btn">
+                <div className="btn-txt">Remove</div>
+            </button>
+        </div>
+    );
+};
+
+export default DeleteButton;
