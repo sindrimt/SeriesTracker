dc40628a89a33015c56c5d3479291a5f766b39ee
Marcus Stephan Nordal <marcusn1301@gmail.com>
2023-02-03 22:15:48 +0100

    #105 Added buttons

--- frontend/src/Pages/SettingsPage/Settings.jsx
+++ frontend/src/Pages/SettingsPage/Settings.jsx
@@ -1,7 +1,16 @@
 import axios from "axios";
 import React, { useRef, useState } from "react";
 import Loading from "../../Pages/LoadingPage/Loading";
-import { SettingsOuter, HeaderOuter, Header, SettingsDashboard, SettingsButtons } from "./SettingsStyles";
+import {
+    SettingsOuter,
+    HeaderOuter,
+    Header,
+    DashboardContainer,
+    Dashboard,
+    SettingsButtons,
+    SettingsButton,
+} from "./SettingsStyles";
+import Sidebar from "../../Components/Sidebar/Sidebar.jsx";
 
 const Settings = () => {
     const [loading, setLoading] = useState(false);
@@ -10,13 +19,24 @@ const Settings = () => {
 
     return (
         <>
+            <Sidebar />
             <SettingsOuter>
-                <SettingsDashboard>
-                    <HeaderOuter>
-                        <Header>Settings</Header>
-                    </HeaderOuter>
-                    <SettingsButtons></SettingsButtons>
-                </SettingsDashboard>
+                <DashboardContainer>
+                    <Dashboard>
+                        <HeaderOuter>
+                            <Header>Settings</Header>
+                        </HeaderOuter>
+                        <SettingsButtons>
+                            <SettingsButton>Change Password</SettingsButton>
+                            <SettingsButton>Change Email</SettingsButton>
+                            <SettingsButton>Change Username</SettingsButton>
+                            <SettingsButton>Change Profile Picture</SettingsButton>
+                            <SettingsButton>Change Banner Picture</SettingsButton>
+                            <SettingsButton>Change Bio</SettingsButton>
+                            <SettingsButton>Change Theme</SettingsButton>
+                        </SettingsButtons>
+                    </Dashboard>
+                </DashboardContainer>
             </SettingsOuter>
         </>
     );
