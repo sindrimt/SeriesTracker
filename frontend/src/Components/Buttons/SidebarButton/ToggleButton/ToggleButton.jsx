269739f793101a5d8a0ee197b800eee08fdbc8a5
sindrimt <sindrimt@gmail.com>
2023-02-08 15:03:17 +0100

    Added togglebutton

---
+++ frontend/src/Components/Buttons/SidebarButton/ToggleButton/ToggleButton.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import "./togglebutton.css";
+
+import { useSelector, useDispatch } from "react-redux";
+import { toggleTheme } from "../../../../redux/features/theme/colorThemeSlice";
+
+const ToggleButton = () => {
+    //const colorTheme = useSelector((state) => state.theme.theme);
+    const [color, setColor] = useState("dark");
+
+    const dispatch = useDispatch();
+
+    const handleToggleTheme = () => {};
+
+    return (
+        <div className="toggle-switch">
+            <label>
+                <input
+                    type="checkbox"
+                    onClick={() => {
+                        dispatch(toggleTheme(color));
+                        setColor(color === "dark" ? "light" : "dark");
+                        console.log(color);
+                    }}
+                />
+                <span className="slider"></span>
+            </label>
+        </div>
+    );
+};
+
+export default ToggleButton;
